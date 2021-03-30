1fcafc930dcc017c064ae3d12945fa39e403519c
慕枫 <javakang@qq.com>
2021-03-30 11:57:01 +0800

    重新编译

--- lib/model/flutter_clash_config_model.g.dart
+++ lib/model/flutter_clash_config_model.g.dart
@@ -8,15 +8,15 @@ part of 'flutter_clash_config_model.dart';
 
 FlutterClashConfig _$FlutterClashConfigFromJson(Map<String, dynamic> json) {
   return FlutterClashConfig(
-    port: json['port'] as int,
+    port: json['port'] as int?,
     mode: _stringToMode(json['mode'] as String),
-    allowLan: json['allow-lan'] as bool,
-    tproxyPort: json['tproxy-port'] as int,
-    mixedPort: json['mixed-port'] as int,
-    socksPort: json['socks-port'] as int,
-    redirPort: json['redir-port'] as int,
-    logLevel: json['log-level'] as String,
-    ipv6: json['ipv6'] as bool,
+    allowLan: json['allow-lan'] as bool?,
+    tproxyPort: json['tproxy-port'] as int?,
+    mixedPort: json['mixed-port'] as int?,
+    socksPort: json['socks-port'] as int?,
+    redirPort: json['redir-port'] as int?,
+    logLevel: json['log-level'] as String?,
+    ipv6: json['ipv6'] as bool?,
   );
 }
 
